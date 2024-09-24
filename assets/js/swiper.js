2801bce1d886263b3684ea330ddfb266e7814a2f
yassine <yassinechmarekh88@gmail.com>
2024-09-24 15:23:40 +0100

    add scrollbar sponsors

---
+++ assets/js/swiper.js
@@ -0,0 +1,11 @@
+const sponsors_swp = new Swiper(".sponsors-swp", {
+  loop: true,
+  slidesPerView: 4,
+  speed: 2000,
+  autoplay: {
+    enabled: true,
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  cssMode: false,
+});
