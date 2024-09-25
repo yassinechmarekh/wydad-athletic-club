f318074328ec342e104505eb7bcfe5dd37df9c4e
yassine <yassinechmarekh88@gmail.com>
2024-09-25 12:28:39 +0100

    add palmares section to homeome page

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -23,3 +23,33 @@ const sponsors_swp = new Swiper(".sponsors-swp", {
     },
   },
 });
+
+
+const palmares_swp = new Swiper(".palmares-swp", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBeetween:20,
+  // autoplay: {
+  //   delay: 2500,
+  // },
+  breakpoints: {
+    1800: {
+      slidesPerView: 6,
+    },
+    1450: {
+      slidesPerView: 5,
+    },
+    991: {
+      slidesPerView: 4,
+    },
+    675: {
+      slidesPerView: 3,
+    },
+    500: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+});
