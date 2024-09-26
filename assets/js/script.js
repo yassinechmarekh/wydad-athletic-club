c87228bb6a60cecc1322c8465d706a8595e8d2bf
yassine <yassinechmarekh88@gmail.com>
2024-09-26 15:48:52 +0100

    custom scrollbar and top navbar and add loader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 // open & close cart
 cart = document.getElementById("cart");
-bg_overlay = document.querySelector('.bg-overlay');
+bg_overlay = document.querySelector(".bg-overlay");
 
-function open_cart(){
-    cart.classList.add("active");
-    bg_overlay.classList.add("active");
+function open_cart() {
+  cart.classList.add("active");
+  bg_overlay.classList.add("active");
 }
 
-function close_cart(){
-    cart.classList.remove("active");
-    bg_overlay.classList.remove("active");
+function close_cart() {
+  cart.classList.remove("active");
+  bg_overlay.classList.remove("active");
 }
+
+// loader
+window.addEventListener("load", function () {
+  document
+    .getElementById("loader-container")
+    .style.setProperty("display", "none", "important");
+  document.body.classList.remove("no-scroll");
+});
+document.body.classList.add("no-scroll");
